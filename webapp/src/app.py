c6a01795d6a9191226008e5c2e04ac6284b1a802
Shotforever1! <peanutchou@thinktronltd.com>
2023-06-19 18:50:08 +0800

    First finished

--- webapp/src/app.py
+++ webapp/src/app.py
@@ -1,5 +1,7 @@
 import requests
+import os
 from fastapi import FastAPI, HTTPException
+import asyncio
 
 app = FastAPI()
 
@@ -7,14 +9,15 @@ app = FastAPI()
 @app.get("/")
 def hello():
     # TODO: Replace the message below with the value of a configuration parameter
-    return {"message": "Hello, World!"}
+    # Get from deployment.yaml's ENV setting
+    return {"message": os.environ['ROOT_MESSAGE']}
 
-
-@app.get("/data")
-def get_star_wars_data():
+# Add query param
+@app.get("/data/{id}")
+def get_star_wars_data(id:int):
     try:
         # TODO: Replace the "1" in the URL below with the value of a query parameter
-        response = requests.get("https://swapi.dev/api/people/1", timeout=5)
+        response = requests.get(f"https://swapi.dev/api/people/{id}", timeout=5)
         response.raise_for_status()
         return response.json()
     except requests.exceptions.HTTPError as e:
@@ -26,8 +29,33 @@ def get_star_wars_data():
 
 
 # TODO: Add new endpoint to return the top 20 people in the Star Wars API with the highest BMI.
-
-
+@app.get("/top-people-by-bmi")
+def get_star_wars_data_by_bmi():
+    try:
+        # Better to use asyncio to async requests if can know the amount of pages
+        result = []
+        this_res = requests.get(f"https://swapi.dev/api/people", timeout=10).json()
+        data = [*this_res["results"]]
+        while True:
+            this_res = requests.get(this_res["next"], timeout=10).json()
+            data = [*data,*this_res["results"]]
+            if this_res["next"] == None:
+                break
+        # there's seperator & "unknown" value in mass/height, remember to clean the data
+        # more readable:
+        for each in data:
+            if each["mass"] != "unknown" and each["height"] != "unknown":
+                each = {**each, "bmi":round(float(each["mass"].replace(",","")) / (float(each["height"].replace(",","")) / 100) ** 2, 2)}
+                result = [*result, each]
+        # lambda style
+        # result = list(map(lambda obj: {**obj, "bmi": round(float(obj["mass"].replace(",","")) / (int(float["height"].replace(",","")) / 100) ** 2, 2)} if obj["mass"] != "unknown" and obj["height"] != "unknown" else obj, result))
+        return sorted(result, key=lambda x: x["bmi"], reverse=True)[:20]
+    except requests.exceptions.RequestException as e:
+        raise HTTPException(status_code=503, detail="Service Unavailable")
+    except (KeyError, ValueError) as e:
+        print(e)
+        raise HTTPException(status_code=500, detail="Data Processing Error")
+    
 if __name__ == "__main__":
     import uvicorn
     
--- webapp/tests/test_get_star_wars_data.py
+++ webapp/tests/test_get_star_wars_data.py
@@ -1,11 +1,36 @@
 from fastapi.testclient import TestClient
-
+import os
 from webapp.src.app import app
+client = TestClient(app)
 
 def test_get_star_wars_data():
-    client = TestClient(app)
-    response = client.get("/data")
+    response = client.get("/data/1")
     assert response.status_code == 200
     assert "name" in response.json()
 
 # Additional test cases can be added by the candidate
+def test_root_api():
+    # Test case for API HTTP error. For example, fetch a Star Wars character with an ID that doesn't exist.
+    response = client.get("/")
+    assert response.status_code == 200
+    assert "message" in response.json() and response.json()["message"] == os.environ['ROOT_MESSAGE']
+
+def test_data_root_api():
+    response = client.get("/data")
+    assert response.status_code == 404
+    assert "detail" in response.json()
+
+def test_data_not_exist_api():
+    response = client.get("/data/invalid")
+    assert response.status_code == 422
+    assert "detail" in response.json()
+
+def test_data_id_not_int():
+    response = client.get("/data/abc")
+    assert response.status_code == 422
+    assert "detail" in response.json()
+
+def test_bmi_api():
+    response = client.get("/top-people-by-bmi")
+    assert response.status_code == 200
+    assert response.json() != []
